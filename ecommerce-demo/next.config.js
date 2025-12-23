e4be1759e97b83fe11d84249111c49b7a42895b1
Tuval Simha <tuval.simha@gmail.com>
2025-12-23 16:26:56 +0200

    fixed

--- ecommerce-demo/next.config.js
+++ ecommerce-demo/next.config.js
@@ -7,8 +7,19 @@ module.exports = {
         protocol: 'https',
         hostname: 'picsum.photos',
         pathname: '/seed/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+        pathname: '/**'
       }
-    ]
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    loader: 'default',
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: false,
+    contentDispositionType: 'inline'
   },
   async rewrites() {
     return [
